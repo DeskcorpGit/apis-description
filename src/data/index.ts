ec2ad4bd95c79d5461c4be1879732b0be775a292
DeskcorpOtavio <otavio.araujo@deskcorp.com.br>
2026-07-08 17:04:50 -0300

    feat: generate OpenAPI schema and integration service handlers for Pix API operations

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,5 @@
 import { basaApiSections } from "./basa-drop1-apis"
 import { basaApiSectionsDrop2 } from "./basa-drop2-apis"
+import { pixApiSections } from "./pix-apis"
 
-export const allApiSections = [...basaApiSections, ...basaApiSectionsDrop2]
+export const allApiSections = [...basaApiSections, ...basaApiSectionsDrop2, ...pixApiSections]
