a145c2cda311066b716c78f6618792d66a89ecd4
DeskcorpOtavio <otavio.araujo@deskcorp.com.br>
2026-07-24 10:20:59 -0300

    feat: add Apis configuration list for CoreBanx services

--- src/links/Api.ts
+++ src/links/Api.ts
@@ -1,8 +1,21 @@
-export const Apis = [
+interface ApisProps {
+  title: string;
+  company: string;
+  function: string;
+  url: string;
+}
+
+export const Apis: ApisProps[] = [
   {
     title: 'OnboardOS API',
     company: 'CoreBanx',
     function: 'Onboarding',
     url: 'https://uat-onboarding.corebanxapp.com.br/docs/index.html#/',
+  },
+  {
+    title: 'Paymentos API',
+    company: 'CoreBanx',
+    function: 'Payments',
+    url: 'https://uat.corebanxapp.com.br/paymentos/docs/index.html#/',
   },
 ];
